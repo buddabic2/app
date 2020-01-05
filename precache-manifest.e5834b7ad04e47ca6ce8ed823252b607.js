311508c05e8996f4bb7d376b75de54a1bb3c10c0
Dawid <dawid.budaszewski1@gmail.com>
2020-01-05 12:26:13 +0000

    Updates

---
+++ precache-manifest.e5834b7ad04e47ca6ce8ed823252b607.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b27b12b58c60315a3f9cd937b4d6bbfe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6685cc4ef9d3a57702bf",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7e43df44f0afa1070f57",
+    "url": "/static/js/2.b9ee7182.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.b9ee7182.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6685cc4ef9d3a57702bf",
+    "url": "/static/js/main.477b0fce.chunk.js"
+  },
+  {
+    "revision": "b45713355acf61d40e30",
+    "url": "/static/js/runtime-main.ee8a1ed5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
